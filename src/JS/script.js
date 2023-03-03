1ae24ad7db19be48811307ff50c012489a0d82a7
Wunna Oo <115157597+WunnaO@users.noreply.github.com>
2023-03-03 22:09:42 +0630

    push commit

---
+++ src/JS/script.js
@@ -0,0 +1,14 @@
+document.querySelector("#btn-menu").onclick = (e) => {
+  let list = document.querySelector("ul");
+  if (e.target.parentElement.classList.contains("close")) {
+    e.target.parentElement.classList.remove("close");
+    document.querySelector("#open").style.display = "none";
+    document.querySelector("#close").style.display = "block";
+    list.classList.add("top-[90px]");
+  } else {
+    e.target.parentElement.classList.add("close");
+    document.querySelector("#open").style.display = "block";
+    document.querySelector("#close").style.display = "none";
+    list.classList.remove("top-[90px]");
+  }
+};
