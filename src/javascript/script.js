328fd8ca24d42d07941aa14f33aeb1809eabf060
Wunna Oo <115157597+WunnaO@users.noreply.github.com>
2023-03-15 14:26:54 +0630

    add js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,16 +1,16 @@
-// const B_menu = document.getElementById("btn-menu");
+const B_menu = document.getElementById("btn-menu");
 
-// B_menu.onclick = (e) => {
-//   let list = document.querySelector("ul");
-//   if (e.target.parentElement.classList.contains("close")) {
-//     e.target.parentElement.classList.remove("close");
-//     document.querySelector("#open").style.display = "none";
-//     document.querySelector("#close").style.display = "block";
-//     list.classList.add("top-[90px]");
-//   } else {
-//     e.target.parentElement.classList.add("close");
-//     document.querySelector("#open").style.display = "block";
-//     document.querySelector("#close").style.display = "none";
-//     list.classList.remove("top-[90px]");
-//   }
-// };
+B_menu.onclick = (e) => {
+  let list = document.querySelector("ul");
+  if (e.target.parentElement.classList.contains("close")) {
+    e.target.parentElement.classList.remove("close");
+    document.querySelector("#open").style.display = "none";
+    document.querySelector("#close").style.display = "block";
+    list.classList.add("top-[90px]");
+  } else {
+    e.target.parentElement.classList.add("close");
+    document.querySelector("#open").style.display = "block";
+    document.querySelector("#close").style.display = "none";
+    list.classList.remove("top-[90px]");
+  }
+};
